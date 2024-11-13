6ee88ab99a774b930fde6b577cb484430d3c6567
JunSaif <douidysifeddine@gmail.com>
2024-11-13 19:44:30 +0100

    init

---
+++ src/main/java/com/example/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ApplicationConfig {
+    // This class is used to activate Faces with CDI.
+}
---
+++ src/main/java/com/example/beans/UserBean.java
@@ -0,0 +1,31 @@
+package com.example.beans;
+
+import com.example.model.User;
+import com.example.service.UserService;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+
+@Named
+@RequestScoped
+public class UserBean implements Serializable {
+
+    @Inject
+    private UserService userService;
+
+    private List<User> users;
+
+    @PostConstruct
+    public void init() {
+        users = userService.getAllUsers();
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+}
+
